06f0bd0d18ec829b638e95ee9fc571a8697d1316
Sadeepa Bandara <janith@Sadeepas-Mini.lan>
2022-05-03 21:05:13 -0500

    Sending all params and end point description

--- server.js
+++ server.js
@@ -7,21 +7,14 @@ const stripe = require('stripe')(process.env.STRIPE_API_KEY)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+// This POST API recieves connect account configuration variables and creates an account.
+// This returns newly created account's id.
 app.post('/createConnectedAccount', async (req, res) => {
   const params = req.body
   let statusCode, response 
   console.log(params)
   try {
-    const account = await stripe.accounts.create({
-      type: params.type,
-      country: params.country,
-      email: params.email,
-      // capabilities: {
-      //   card_payments: {requested: true},
-      //   transfers: {requested: true},
-      // },
-    })
+    const account = await stripe.accounts.create(params)
     statusCode = 200
     response = {accountId: account.id}
   } catch(e) { 
